f83673c3ad25f52364e69b921d57cfc41a384456
Vitalii@DESKTOP-6BUON5E <opaynitch@ukr.net>
2025-09-11 17:21:34 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <AppHeader />
+  <h1 class="text-red-500 text-2xl">Hello Tailwind 👋</h1>
+  <div ref="box" class="w-32 h-32 bg-green-500 rounded-lg"></div>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { gsap } from 'gsap'
+import AppHeader from './components/AppHeader.vue'
+
+const box = ref(null)
+
+onMounted(() => {
+  gsap.to(box.value, {
+    x: 200,
+    rotation: 360,
+    duration: 2,
+    ease: 'power2.inOut',
+  })
+})
+</script>
+
+<style scoped></style>
